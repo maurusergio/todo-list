b93d36b57be8dd59e841437ef2a88f3c945d2f18
Mauro Sergio <maurusergio@gmail.com>
2022-06-19 17:02:13 -0300

    initial commit

--- w7/settings.py
+++ w7/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'w7.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.postgresql',
     }
 }
 
