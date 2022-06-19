20a19bc8d175fbae4fcd0b1a92c4cc46c9ffc5f1
Mauro Sergio <maurusergio@gmail.com>
2022-06-19 19:06:16 -0300

    initial commit

--- w7/views.py
+++ w7/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Home Page | <a href="/admin">login</a><p><a href="/todolist">To-do List App</a></p>')
+    return HttpResponse('Home Page | <a href="/admin">login</a><p><a href="/todo-list-maurosergio">To-do List App</a></p>')
